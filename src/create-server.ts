5fe9abd2cc46c10983752c25efb1e3eb54cc790c
Andrew Cole <andrew.thielcole@gmail.com>
2025-10-10 15:37:32 -0700

    Resource domains are valid

--- src/create-server.ts
+++ src/create-server.ts
@@ -53,7 +53,7 @@ export const createServer = (plaidClient: PlaidApi) => {
     "openai/widgetPrefersBorder": true,
     "openai/widgetCSP": {
       connect_domains: [],
-      resource_domains: ["personal-finance-mcp.vercel.app"]
+      resource_domains: ["https://personal-finance-mcp.vercel.app"]
     }
   };
 
